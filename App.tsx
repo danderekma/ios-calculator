76f0ef95751babda7761a2f179a54841d973491f
Derek Ma <danderekma@gmail.com>
2023-06-21 17:11:40 +0800

    Install mathjs

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+import { useReducer } from "react";
 import { Text, View } from "react-native";
 
 import "./styles.css";
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-500">
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
